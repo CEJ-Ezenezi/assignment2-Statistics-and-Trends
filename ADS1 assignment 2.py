ccd92205acc42b375ff6774962696a5043640663
CEJ-Ezenezi <chiamakaezenezi1@gmail.com>
2023-12-13 23:36:00 +0000

    complete code for the project

--- ADS1 assignment 2.py
+++ ADS1 assignment 2.py
@@ -1,46 +1,47 @@
-# -*- coding: utf-8 -*-+
-"""original_data?.,
+"""
 
 Created on Sun Dec  3 19:41:24 2023
 
 @author: CHIAMAKA
 """
-
+#importing libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import stats
+from scipy.stats import kurtosis
+from scipy.stats import skew
 
-#Defining functions
 
+
+#Defining relevant functions
 def read_data(csv_filename):
     """reads a csv file into a dataframe
     and returns the original dataframe df1 and the
-    transposed dataframe, df_T
+    cleaned transposed dataframe, df_T
     """
     
     #read original csv file into pandas dataframe
     df1 = pd.read_csv(csv_filename)
 
-    #setting index
+    #setting multi-index
     df1 = df1.set_index(["Series Name","Country Name"])
     
     #cleaning the data, drop irrelevant columns
     df1 = df1.drop(["Series Code", "Country Code"], axis=1)
-    #drop last two rows with Nan values
+    #drop last rows with Nan values
     df1 = df1.dropna()
     #get rid of '..' from the data
     df1 = df1.replace('..', np.nan)
-    #assigning appropriate data type
+    #assigning appropriate data type tothe dataframe
     df1 = df1.astype("float")
     #rename columns
-    df1.columns = ["1990", "2000", "2010", "2011", "2012", "2013", 
-                          "2014", "2015", "2016", "2017", "2018", "2019", 
-                          "2020", "2021"]
+    df1.columns = ["2010", "2011", "2012", "2013", "2014", "2015", 
+                   "2016", "2017", "2018", "2019", "2020", "2021"]
 
     #transposes the dataframe
     df_T = df1.transpose()
+    #rename long column names
     df_T.rename(columns={"Agriculture, forestry, and fishing, value added (% of GDP)":
     "Agriculture(% of GDP)", 
     "Unemployment, total (% of total labor force) (national estimate)":
@@ -53,7 +54,8 @@ def read_data(csv_filename):
 def lineplot(df, country_to_plot, y):
     """ Function to create a lineplot. 
     Arguments:
-    A dataframe with columns "country" and index "x"
+    A dataframe with multi-index columns "country"  at level 1 and 
+    columns "x" at level 0
     A list containing values of a column to iterate over to plot.
     A label for the y axis "y"
 
@@ -66,9 +68,9 @@ def lineplot(df, country_to_plot, y):
 
     plt.xlabel("year")
     plt.ylabel(y)
-    #removing white space left and right.
     
-    plt.legend(loc='upper right', bbox_to_anchor=(0, 0))
+    
+    plt.legend(loc='upper right', bbox_to_anchor=(1, 1))
     plt.show()
     
     return plt.figure()
@@ -104,38 +106,30 @@ def plot_correlation_heatmap(country_data, title):
 
 
 
-def plot_bubble_chart(data, indicator1, indicator2, indicator3, size_variable, title="Bubble Chart"):
-    """
-    Plot a bubble chart for three economic indicators for different countries.
-
-    Parameters:
-    - data: DataFrame containing the economic indicators for different countries.
-    - indicator1, indicator2, indicator3: Names of the economic indicators.
-    - size_variable: Name of the variable to be used for bubble size.
-    - title: Title of the plot.
-    """
-
+def plot_bubble_chart(data, x_indicator, y_indicator, size_indicator, title):
     plt.figure(figsize=(10, 8))
+    
+    for country in data.columns.levels[1]:
+        x_values = data[x_indicator][country].mean()
+        y_values = data[y_indicator][country].mean()
+        size_values = data[size_indicator][country].mean()
 
-    # Scatter plot with bubble size determined by the third variable
-    plt.scatter(data[indicator1], data[indicator2], s=data[size_variable], alpha=0.7, cmap='viridis', c=data[indicator3])
+        plt.scatter(x_values, y_values, s = np.log(size_values) * 10 
+            #adjusted the size_values so that the bubbles are visible
+            ,alpha=0.7, edgecolors="k", cmap="viridis",label=country)
 
-    # Add labels and title
-    plt.xlabel(indicator1)
-    plt.ylabel(indicator2)
-    plt.title(title)
+    plt.title(f'Bubble Chart: {x_indicator} vs {y_indicator} by {size_indicator}')
+    plt.xlabel(x_indicator)
+    plt.ylabel(y_indicator)
 
-    # Add a colorbar to represent the third economic indicator
-    cbar = plt.colorbar()
-    cbar.set_label(indicator3)
-
-    # Show the plot
+    plt.legend(markerscale=0.5)  
+    plt.grid(True)
     plt.show()
 
 
 
 #calling the read_data function
-csv_filename = "eco_activity2.csv"
+csv_filename = "world_eco_activity.csv"
 original_data, transposed_data = read_data(csv_filename)
 print("Original Data:")
 print(original_data)
@@ -143,48 +137,39 @@ print(original_data)
 print("\nTransposed Data:")
 print(transposed_data)
 
+#three summary statistics to explore the dataframe
+print(transposed_data.describe())
+print(transposed_data["Agriculture(% of GDP)"].describe().transpose())
+data_Tstat = [skew(transposed_data), kurtosis(transposed_data)]
+print(data_Tstat)
+
+#checking the skewness and kurtosis of the distribution
+GDP_stat = [skew(transposed_data["GDP per capita growth (annual %)"]),
+              kurtosis(transposed_data["GDP per capita growth (annual %)"])]
+print(GDP_stat)
+country_to_plot = ["Brazil", "China", "Canada",	"Germany","India","South Africa"
+                   ,"Nigeria", "New Zealand", "United States", "United Kingdom"
+                   ,"South Africa", "Russian Federation"]
 
 
-#calling the line_plot function on the GDP for each country
-df_GDP = transposed_data["GDP (current LCU)"]
-print(df_GDP)
-
-country_to_plot = ["Algeria", "Australia", "Brazil", "Cameroon", 
-                   "China", "Canada",	"Ghana", "France",	"Germany",	
-                   "Egypt, Arab Rep.","India","Japan", "Jamaica","South Africa"
-                   ,"New Zealand", "Nigeria", "Russian Federation", "Turkiye"
-                   , "United States", "United Kingdom", "United Arab Emirates"
-                   , "Zimbabwe"]
-
-#calling the linplot function on the GDP for each country
-lineplot(df_GDP, country_to_plot, y="GDP")
-
-#calling the lineplot function on the total population of each country
-df_pop = transposed_data["Population, total"]
-lineplot(df_pop, country_to_plot, y="Population")
-
-#for series in transposed_data.columns.levels[0]:
-    #df_pop = transposed_data[series]
-    #lineplot(df_pop, country_to_plot, y="series")
+#calling the lineplot function on the GDP for each country
+for series in transposed_data.columns.levels[0]:
+    df_by_indicator = transposed_data[series]
+    lineplot(df_by_indicator, country_to_plot, y=series)
 
 
 
 #plotting correlation heatmap for the economic indicators of each country
-#for country in country_to_plot:
-    #sliced_data = transposed_data.xs(country, level="Country Name", axis=1)
-    #plot_correlation_heatmap(sliced_data, title=country)
-    
 for country in country_to_plot:
-    sliced_data = transposed_data.xs(country, level="Country Name", axis=1)
-    plot_bubble_chart(transposed_data, "GDP per capita (current LCU)", 
-            "CO2 emissions (kg per PPP $ of GDP)", "Population, total",
-            size_variable="Population, total", title=country)
+     sliced_data = transposed_data.xs(country, level="Country Name", axis=1)
+     plot_correlation_heatmap(sliced_data, title=country)
     
-
-
-
-#three summary statistics
-transposed_data.describe()    
-data_Tstat = transposed_data.agg([stats.skew, stats.kurtosis])
+#plotting the bubble chart for three economic indicator
     
+plot_bubble_chart(transposed_data, "GNI per capita growth (annual %)"
+                  ,"GDP per capita growth (annual %)", "Population, total",
+                  "Bubble Chart")
+
+
+
     
